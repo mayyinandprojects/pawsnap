ff8f997a402de0da1785486df39bcd356e1c2a31
may_ <lammayyin@gmail.com>
2025-02-08 23:39:51 +0100

    feat: add about page

---
+++ pawsnap/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <>
+        <div className="home-page"></div>
+            <h1>About Pawsnap</h1>
+            <p>Pawsnap is a web application used to generate animal images. </p>
+            <AnimalSelector />
+        </>
+    )
+}
+
+export default About;
