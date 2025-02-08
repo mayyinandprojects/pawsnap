c56c89910c56ddd66501ed1fe9cc5a02cb77df86
may_ <lammayyin@gmail.com>
2025-02-08 23:35:55 +0100

    test: make sure home and about links are in navigation bar

--- pawsnap/src/components/__tests__/Navbar.test.jsx
+++ pawsnap/src/components/__tests__/Navbar.test.jsx
@@ -1,7 +1,12 @@
+//pawsnap\src\components\__tests__\Navbar.test.jsx
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "../Navbar";
+
+//keep mock file on top this ensures Jest properly mocks the PNG import before rendering.
+jest.mock("../../assets/pawsnap-logo.png", () => "mock-logo.png");
 
 describe("Navbar Component", () => {
   test("renders the navigation bar with logo and About button", () => {
@@ -10,11 +15,11 @@ describe("Navbar Component", () => {
         <Navbar />
       </BrowserRouter>
     );
+
+    expect(screen.getByRole("link", { name: /pawsnap/i })).toBeInTheDocument();
+
+    expect(screen.getByRole("link", { name: /about/i })).toBeInTheDocument();
   });
-
-  expect(screen.getByRole("link", { name: /pawsnap/i })).toBeInTheDocument();
-
-  expect(screen.getByRole("link", { name: /about/i })).toBeInTheDocument();
 
   test("navigates to About page when About button is clicked", async () => {
     render(
@@ -25,6 +30,6 @@ describe("Navbar Component", () => {
 
     await userEvent.click(screen.getByRole("link", { name: /about/i }));
 
-    expect(screen.getByRole("link", { name: /about/i })).tobeInTheDocument();
+    expect(screen.getByRole("link", { name: /about/i })).toBeInTheDocument();
   });
 });
