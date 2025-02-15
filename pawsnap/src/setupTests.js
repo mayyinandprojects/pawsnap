0f62976acd0e1241239271231c5fc13bcfaa9a1b
may_ <lammayyin@gmail.com>
2025-02-15 16:18:49 +0100

    chore: configure jest to use node fetch polyfill to avoid fetch is undefined error

--- pawsnap/src/setupTests.js
+++ pawsnap/src/setupTests.js
@@ -2,5 +2,12 @@
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from "util";
 
+// Only import `node-fetch` if running in Jest (Node.js)
+if (typeof global !== "undefined") {
+  const fetch = require("node-fetch");
+  global.fetch = fetch;
+}
+
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
