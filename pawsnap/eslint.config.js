82540f48de0c7285deb1a4fa2d1bc80257dd3ba5
may_ <lammayyin@gmail.com>
2025-02-15 16:07:06 +0100

    chore: add configurations to es lint to work with jest

--- pawsnap/eslint.config.js
+++ pawsnap/eslint.config.js
@@ -1,8 +1,10 @@
+// pawsnap\eslint.config.js
 import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jest from 'eslint-plugin-jest'
 
 export default [
   { ignores: ['dist'] },
@@ -10,7 +12,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.jest,  // Adds Jest globals
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -22,12 +27,18 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      jest,  // Include the jest plugin
+    },
+    env: {
+      jest: true, // Enable Jest globals
+      browser: true, // Enable browser globals
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...jest.configs.recommended.rules, // Add Jest plugin's recommended rules
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
