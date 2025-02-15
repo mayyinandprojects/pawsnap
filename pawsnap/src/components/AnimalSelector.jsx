a2b8247aa2ebc7e1ae92518b776e4e0ed20a24bf
may_ <lammayyin@gmail.com>
2025-02-15 16:16:00 +0100

    add: animal selector component
    section for users to choose an animal - cat, dog or fox

--- pawsnap/src/components/AnimalSelector.jsx
+++ pawsnap/src/components/AnimalSelector.jsx
@@ -1,24 +1,39 @@
 // pawsnap\src\components\AnimalSelector.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useFetchImage from "../hooks/useFetchImage";
 
-
 function AnimalSelector() {
-  const [selectedAnimal, setSelectedAnimal] = useState("CAT"); 
+  const [selectedAnimal, setSelectedAnimal] = useState("CAT");
   const [image, fetchImage] = useFetchImage(selectedAnimal);
-
+  useEffect(() => {
+    fetchImage(); // Fetch an image when the hook is used
+  }, [selectedAnimal]); // Refetch when the animal type changes
 
   return (
     <>
-      <button onClick={() => setSelectedAnimal("CAT")}>😺 Cat</button>
-      <button onClick={() => setSelectedAnimal("DOG")}>🐶 Dog</button>
-      <button onClick={() => setSelectedAnimal("FOX")}>🦊 Fox</button>
+      <button data-testid="select-cat"  className={selectedAnimal === "CAT" ? "selected" : ""} onClick={() => setSelectedAnimal("CAT")}>
+        😺 Cat
+      </button>
+      <button data-testid="select-dog"  className={selectedAnimal === "DOG" ? "selected" : ""} onClick={() => setSelectedAnimal("DOG")}>
+        🐶 Dog
+      </button>
+      <button data-testid="select-fox"  className={selectedAnimal === "FOX" ? "selected" : ""}onClick={() => setSelectedAnimal("FOX")}>
+        🦊 Fox
+      </button>
 
       {/* Show the image */}
-      {image && <img className = "animalImage" src={image} alt={`A random ${selectedAnimal.toLowerCase()}`} />}
-      
+      {image && (
+        <img
+          className="animalImage"
+          src={image}
+          alt={`A random ${selectedAnimal.toLowerCase()}`}
+        />
+      )}
+
       {/* Fetch a new image on click */}
-      <button onClick={fetchImage}>🔄 Generate {selectedAnimal.toLowerCase()} pictures</button>
+      <button onClick={fetchImage}>
+        🔄 Generate {selectedAnimal.toLowerCase()} pictures
+      </button>
     </>
   );
 }
