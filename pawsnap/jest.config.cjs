66d8e9318e3a5071292b9c4a6a4f8b0929d9c759
may_ <lammayyin@gmail.com>
2025-02-15 16:10:29 +0100

    chore: add configurations to jest config file

--- pawsnap/jest.config.cjs
+++ pawsnap/jest.config.cjs
@@ -1,14 +1,12 @@
-// pawsnap\jest.config.cjs
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest', // Transforms JavaScript and JSX files using Babel
-    },
-    moduleNameMapper: {
-      '\\.(css|less|scss)$': 'identity-obj-proxy', // Mock CSS imports
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], // Setup file for Jest DOM
-    testMatch: ['**/__tests__/**/*.test.jsx'], // Pattern to find test files
-    moduleDirectories: ['node_modules', 'src'] // Allow absolute imports from ‘src’
-  };
-  
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.jsx?$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }]
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testMatch: ['**/__tests__/**/*.test.jsx'],
+  moduleDirectories: ['node_modules', 'src']
+};
